72bf671615ff4601cac90c53d394b5554ccd0603
lixiaoyu <lixiaoyu@xmfunny.com>
2023-08-15 17:35:07 +0800

    <feature> 恢复Overlay相机的阴影开关 但是不起作用 Overlay相机不开启阴影

--- Editor/Camera/UniversalRenderPipelineCameraUI.Rendering.Drawers.cs
+++ Editor/Camera/UniversalRenderPipelineCameraUI.Rendering.Drawers.cs
@@ -112,8 +112,7 @@ namespace UnityEditor.Rendering.Universal
                 PostProcessingWarningDrawer,
                 CED.Group(
                     DrawerRenderingClearDepth,
-                    // Overlay 相机强行关闭阴影渲染（FunnyRender中对阴影Pass进行判断）
-                    //DrawerRenderingRenderShadows,
+                    DrawerRenderingRenderShadows,
                     DrawerRenderingDepthTexture
                 )
             );
