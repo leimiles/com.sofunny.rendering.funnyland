9be0e8356a9a6ad202bcefe74cf284ba1fe4d99b
lixiaoyu <lixiaoyu@xmfunny.com>
2023-08-23 19:59:02 +0800

    <feature> 默认高低管线配置

--- Runtime/Data/Funnyland/GraphicQuality/GraphicQualitySettings.cs
+++ Runtime/Data/Funnyland/GraphicQuality/GraphicQualitySettings.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using SoFunny.Rendering.Funnyland;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using Debug = System.Diagnostics.Debug;
 
 namespace SoFunny.Rendering.Funnyland {
+    public enum GraphicQuality {
+        High,
+        Low,
+        Custom
+    }
     public static class GraphicQualitySettings {
-        public static void SetQualityPrefab(GraphicQualitySettingData data) {
+        public static void SetQualityPrefab(ref GraphicQualitySettingData data) {
+            if (data == null) {
+                return;
+            }
             var asset = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
-            ShadowSetting(asset, data.shadow);
+            //ShadowSetting(asset, data.shadow);
             ShadingQuality();
             PostSetting(asset, data.post);
             DecalSetting();
@@ -53,5 +63,45 @@ namespace SoFunny.Rendering.Funnyland {
                 QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
             }
         }
+
+        /// <summary>
+        /// 设置高配置数据
+        /// </summary>
+        public static void SetHighQualitySetting(ref GraphicQualitySettingData data) {
+            data.post = true;
+            data.shadow = true;
+            data.globalTextureMipmapLevel = GlobalTextureMipmapLevel.Full;
+            data.anisotropicTexture = true;
+            SetQualityPrefab(ref data);
+        }
+        
+        /// <summary>
+        /// 设置低配置数据
+        /// </summary>
+        public static void SetLowQualitySetting(ref GraphicQualitySettingData data) {
+            data.post = false;
+            data.shadow = false;
+            data.globalTextureMipmapLevel = GlobalTextureMipmapLevel.Half;
+            data.anisotropicTexture = false;
+            SetQualityPrefab(ref data);
+        }
+
+        /// <summary>
+        /// 根据机型初始化默认配置以及设置高低配置
+        /// </summary>
+        public static void SetDefaultQualitySetting(GraphicQuality quality, ref GraphicQualitySettingData data) {
+            switch (quality) {
+                case GraphicQuality.High:
+                    SetHighQualitySetting(ref data); 
+                    break;
+                case GraphicQuality.Low:
+                    SetLowQualitySetting(ref data);
+                    break;
+                default:
+                    SetQualityPrefab(ref data);
+                    break;
+            } 
+        }
+        
     }
 }
