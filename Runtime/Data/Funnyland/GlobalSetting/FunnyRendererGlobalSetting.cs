ec1e9d4e3850459f1ff53db0296873e1e85dad6c
崔健 <cuijian@xmfunny.com>
2024-01-22 17:05:44 +0800

    <feature> 添加feature开关设置

---
+++ Runtime/Data/Funnyland/GlobalSetting/FunnyRendererGlobalSetting.cs
@@ -0,0 +1,20 @@
+﻿using SoFunny.Rendering.Funnyland;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace SoFunny.Rendering.Funnyland
+{
+    public static class FunnyRendererGlobalSetting
+    {
+        static ScriptableRenderer GetRenderer() {
+            UniversalRenderPipelineAsset asset = (UniversalRenderPipelineAsset)GraphicsSettings.currentRenderPipeline;
+            return asset.GetRenderer(0);
+        }
+
+        public static void SetFeatureActive(int index,bool active) {
+            ScriptableRenderer renderer = GetRenderer();
+            renderer.rendererFeatures[index].SetActive(active);
+        }
+    }
+}
